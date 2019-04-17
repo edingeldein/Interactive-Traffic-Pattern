7c41671dbfd644705ec3eaa5e0179918eec90c2a
edingeldein <erich.dingeldein@und.edu>
2019-04-17 12:13:00 -0500

    UI draws to screen. Camera moves with button clicks.

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Transform CurrentTarget;
+    public bool Move;
+    public float a = 9f;
+    Transform mainCameraTrans;
+    Transform eastCameraTrans;
+    Transform westCameraTrans;
+    float vMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCameraTrans = transform.Find("MainCameraPosition");
+        eastCameraTrans = transform.Find("EastCameraPosition");
+        westCameraTrans = transform.Find("WestCameraPosition");
+        Move = false;
+    }
+
+    void Update()
+    {
+        if (!Move) return;
+
+        MainCamera.transform.position = CurrentTarget.position;
+        MainCamera.transform.rotation = CurrentTarget.rotation;
+
+        Move = (MainCamera.transform.position == CurrentTarget.position &&
+            MainCamera.transform.rotation == CurrentTarget.rotation);
+    }
+
+    public void SetTransform(CameraPosition pos)
+    {
+        if (pos == CameraPosition.Main)
+            CurrentTarget = mainCameraTrans;
+        else if (pos == CameraPosition.East)
+            CurrentTarget = eastCameraTrans;
+        else
+            CurrentTarget = westCameraTrans;
+
+        Move = true;
+    }
+
+}
+
+public enum CameraPosition
+{
+    Main,
+    East,
+    West
+}
+
---
+++ Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Canvas EastCanvas;
+    public Canvas WestCanvas;
+    public CameraController CameraController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var eastBtn = GameObject.Find("btnEast").GetComponent<Button>();
+        var westBtn = GameObject.Find("btnWest").GetComponent<Button>();
+        var mainBtn = GameObject.Find("btnMain").GetComponent<Button>();
+        CameraController = GameObject.Find("CameraPositions").GetComponent<CameraController>();
+
+        eastBtn.onClick.AddListener(() => CameraController.SetTransform(CameraPosition.East));
+        westBtn.onClick.AddListener(() => CameraController.SetTransform(CameraPosition.West));
+        mainBtn.onClick.AddListener(() => CameraController.SetTransform(CameraPosition.Main));
+    }
+
+}
